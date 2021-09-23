a123174f5587fd3327792014e460e68810905b99
Nora Junaid <njunaid@umass.edu>
2021-09-23 12:07:24 -0400

    Final with Pic Shuffling

--- Image Project/ViewController.swift
+++ Image Project/ViewController.swift
@@ -14,19 +14,29 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.text="Where are my cats?"
         
         
     }
 
     @IBAction func displayButton(_ sender: UIButton) {
+        let  helloMessage = "Hello There"
+        
         if messageLabel.text=="Where are my cats?" {
-        messageLabel.text = "Hello There"
+        messageLabel.text = helloMessage
             catImage.image=UIImage(named: "Cat")
             
+        } else if messageLabel.text==helloMessage{
+            messageLabel.text = "Here's another cat"
+            catImage.image=UIImage(named: "Cat2")
+            
+        }else if messageLabel.text=="Here's another cat"{
+            messageLabel.text = "Stars are magic"
+            catImage.image=UIImage(named:"Stars")
+            
+        } else { messageLabel.text="Hello WORLD"
+            
         }
-        
-        else{
-            messageLabel.text="We need more cats"}
        
     }
     
