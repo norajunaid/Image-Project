213a50f01c0ca119efe2a1dd76547bfcef34f089
Nora Junaid <njunaid@umass.edu>
2021-09-22 23:29:20 -0400

    Two Cats Created

--- Image Project/ViewController.swift
+++ Image Project/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var catImage: UIImageView!
+    @IBOutlet weak var catImage2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func displayButton(_ sender: UIButton) {
+        messageLabel.text = "Hello There"
+        catImage.image=UIImage(named: "Cat")
+        catImage2.image=UIImage(named: "Cat2")
+    }
+    
 }
 
